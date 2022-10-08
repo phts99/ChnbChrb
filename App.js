4b6403cbc8b49237480ea6babea25db03d38ae03
phts99 <phts99@gmail.com>
2022-10-08 14:23:17 -0300

    feat(login): add social media icons style component

--- App.js
+++ App.js
@@ -15,15 +15,7 @@ export default function App() {
   return (
     <KeyboardAvoidingView style={styles.background}>
       <View
-        style={{
-          paddingVertical: 15,
-          paddingHorizontal: 10,
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "right",
-          bottom: "1%",
-          left: "42%",
-        }}
+        style={styles.socialMediaIcons}
       >
         <AkarIcons.InstagramFill
           color="#2C9196"
@@ -150,6 +142,15 @@ const styles = StyleSheet.create({
   },
   forgotPasswordText: {
     fontSize: 10,
+  },
+  socialMediaIcons: {
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "right",
+    bottom: "1%",
+    left: "42%",
   },
   containerWelcome: {
     flex: 1,
