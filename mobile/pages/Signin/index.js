f4dd741c8f8640c462e70f56505d0e120b2eb35b
phts99 <phts99@gmail.com>
2022-10-07 21:35:44 -0300

    fix(signin): update login component style content

---
+++ mobile/pages/Signin/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from "./styles";
+
+function Signin() {
+  return <Text> Login page </Text>;
+}
+
+export default Signin;
--- mobile/pages/Signin/styles.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Text } from "react-native";
-
-function Signin() {
-  return <Text> Login page </Text>;
-}
-
-export default Signin;
