a72fac2325f9e8c8c4f14744bc7cc6257ede7347
phts99 <phts99@gmail.com>
2022-10-07 21:32:42 -0300

    feat(login): add login page component

--- mobile/App.js
+++ mobile/App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import Signin from "./pages/Signin/styles";
 
 export default function App() {
   return (
     <>
+      <Signin/>
       <StatusBar style="light" />
     </>
   );
---
+++ mobile/pages/Signin/styles.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from "react-native";
+
+function Signin() {
+  return <Text> Login page </Text>;
+}
+
+export default Signin;
